5fe8fd84fc9c72da2dcfb95b0d32ae0d0d3b5f25
mark <mark.xu@sbdinc.com>
2022-01-28 21:17:21 +0800

    动画1

---
+++ MarkUI/MarkUI.Demo/LampControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarkUI.Demo
+{
+    /// <summary>
+    /// Interaction logic for LampControl.xaml
+    /// </summary>
+    public partial class LampControl : UserControl
+    {
+        public LampControl()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += UpdateRectangle;
+        }
+
+        private void UpdateRectangle(object sender, EventArgs e)
+        {
+            this.GeometryText.Clip = this.MyEllipseGeometry;
+        }
+    }
+}
